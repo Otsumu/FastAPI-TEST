0e8e54f3c48ade4b93906d9098ff269c72f4bb1c
Otsumu <nemo1006@hotmail.co.jp>
2025-07-14 16:29:19 +0900

    server.py index 型見直し

--- backend/server.py
+++ backend/server.py
@@ -14,6 +14,7 @@ summary_db = CreateSummary()
 
 @app.get("/api/metrics")
 def get_metrics(mode: str ="realtime"):
+    """生データの取得"""
     metrics = db.get_metrics(mode)
     return metrics
 
@@ -26,13 +27,15 @@ def post_metrics(json_data: dict):
     else:
         return {"status": "error", "message": "データ挿入に失敗しました"}
 
-# get_summary_dataのデータポイント設定
-# cpu_id: int = None = Union[int, None]と同じ意味
-# cpu_idが整数またはNoneであることを示すためのFastAPIの型ヒント、
-# cpu_idが指定されない場合、Noneがデフォルト値として使用される、全てのCPUの集計データが返される
+
+ #get_summary_dataのデータポイント設定
+ #cpu_id: int = None = Union[int, None]と同じ意味
+ #cpu_idが整数 or Noneであることを示すためのFastAPIの型ヒント、
+ #cpu_idの指定がない場合、Noneをデフォルト値として使用、全てのCPUの集計データが返される(interval_typeも同様)
 @app.get("/api/summary")
 def get_summary_data(start_timestamp: int, end_timestamp: int, 
                      cpu_id: Optional[int] = None, interval_type: Optional[int] = None):
+    """集計加工データの取得"""
     print(f"=== API呼び出し ===")
     print(f"interval_type: {interval_type}")
     summary_data = summary_db.get_summary_data(start_timestamp, end_timestamp, cpu_id, interval_type)
@@ -40,7 +43,7 @@ def get_summary_data(start_timestamp: int, end_timestamp: int,
     return summary_data
 
 @app.post("/api/summary")
-def create_summary(seconds: int = 600, index: str = "10min"):
+def create_summary(seconds: int = 600, index: int = 0):
     created_count = summary_db.create_summary_data(seconds, index)
     if created_count > 0:
         return {"status": "success", "created": created_count}
